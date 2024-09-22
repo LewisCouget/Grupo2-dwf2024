9ad0fa25b8d59ec4354689d94c6b2b28c8864611
LewisCouget <lewis_2051@hotmail.com>
2024-09-22 17:12:40 -0300

    Particiones

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,9 +1,11 @@
-import Header from "./components/Header";
+import Header from "./components/HeaderNav";
+import "./index.css";
 function App() {
 
   return (
     <>
       <Header/>
+      <Reel/>
     </>
   );
 }
