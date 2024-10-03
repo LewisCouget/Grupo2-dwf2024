1a12c7b9c210a489972787ee53f83d56ef00204b
LewisCouget <lewis_2051@hotmail.com>
2024-10-03 20:51:01 -0300

    Add--CountAndCss

--- project/src/components/HeaderNav/ShoppingCart/ShoppingCart.jsx
+++ project/src/components/HeaderNav/ShoppingCart/ShoppingCart.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./ShoppingCart.css";
 import ImgProduct from "../../../images/image-product-1-thumbnail.jpg";
+import ImgDelete from "../../../images/icon-delete.svg";
 
 export const ShoppingCart = () => {
   return (
@@ -15,9 +16,13 @@ export const ShoppingCart = () => {
             $125.00 x 3 <strong>$375.00</strong>
           </p>
         </div>
-        <button className="Remove--item">X</button>
+        <button className="Remove--item">
+          <img src={ImgDelete} alt="" />
+        </button>
       </div>
-      <button className="Checkout--button"></button>
+      <button className="Checkout--button">
+        <strong>Checkout</strong>
+      </button>
     </div>
   );
 };
--- project/src/components/Main/Main.jsx
+++ project/src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "./Main.css";
 import Carrucel from "./Carrusel";
@@ -6,23 +6,35 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ImgMinus from "../../images/icon-minus.svg";
 import ImgPlus from "../../images/icon-plus.svg";
+import ImgCart from "../../images/icon-cart.svg";
+import ImgBig from "../../images/image-product-1.jpg";
 
 export const Main = () => {
-  /*  const [selectedImg, setSelectedImg] = useState(Carrucel[0].image); */
+  const [count, setCount] = useState(0);
+  const handlePlus = () => setCount((prevCount) => prevCount + 1);
+  const handleSubstract = () => {
+    setCount((prevCount) => (prevCount > 0 ? prevCount - 1 : 0));
+  };
   const settings = {
     dots: true,
     infinite: true,
     slidesToShow: 4,
-    /*  afterChange: (index) => setSelectedImg(Carrucel[index].image), */
+  };
+
+  const handleInputChange = (e) => {
+    setCount(e.target.value);
+  };
+
+  const handleAddToCart = (e) => {
+    props.setCartCount(count);
+    setCount(0);
   };
 
   return (
     <main className="Main--content">
-      <div className="Selected--image">
-        {/* <img src={selectedImg} alt="Selected" /> */}
-      </div>
+      <div className="Selected--image"></div>
       <div className="Carrusel--container">
-        <h1>Carrusel</h1>
+        <img src={ImgBig} alt="" className="ImgBig" />
         <Slider {...settings}>
           {Carrucel.map((item, index) => (
             <div key={index}>
@@ -42,20 +54,30 @@ export const Main = () => {
             the weather can offer.
           </p>
         </div>
+
         <div className="Add--Cart__cost">
           <h2>$125.00</h2> 50% $250.00
         </div>
         <div className="Add--Cart__items">
-          <button>
-            <img src={ImgMinus} alt="" />
+          <span className="Add--Counter">
+            <button className="Minus" onClick={handleSubstract}>
+              <img src={ImgMinus} className="ImgMinus" alt="Minus" />
+            </button>
+            <span className="counter" onChange={handleInputChange}>
+              {count}
+            </span>
+            <button className="Plus" onClick={handlePlus}>
+              <img src={ImgPlus} className="ImgPlus" alt="Plus" />
+            </button>
+          </span>
+          <button className="Add--Cart__buttonAdd" onClick={handleAddToCart}>
+            <img src={ImgCart} alt="" className="Add--ImgCart" />{" "}
+            <strong>Add to cart</strong>
           </button>
-          <input type="number" />
-          <button>
-            <img src={ImgPlus} alt="" />
-          </button>
-          <button className="Add--Cart__buttonAdd">Add to cart</button>
         </div>
       </div>
     </main>
   );
 };
+
+export default Main;
